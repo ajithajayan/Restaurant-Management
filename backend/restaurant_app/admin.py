6e963201cef2173a17d1d3a43e8e32c696582342
mohd-shibilicv <project.nasra@gmail.com>
2024-08-25 21:41:03 +0530

    feat: custom admin panel customization

--- backend/restaurant_app/admin.py
+++ backend/restaurant_app/admin.py
@@ -1,16 +1,38 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from restaurant_app.models import *
 
-admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Dish)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(Bill)
-admin.site.register(Notification)
-admin.site.register(Floor)
-admin.site.register(Table)
-admin.site.register(Coupon)
+
+class CustomAdminSite(AdminSite):
+    site_header = "Nasscript"
+    site_title = "Nasscript"
+    index_title = "Welcome to Nasscript Admin Panel"
+
+    def get_app_list(self, request):
+        app_list = super().get_app_list(request)
+        excluded_apps = ['token_blacklist', 'auth']
+        return [app for app in app_list if app['app_label'] not in excluded_apps]
+
+
+admin_site = CustomAdminSite()
+
+admin_site.site_header = "Nasscript"
+admin_site.site_title = "Nasscript"
+admin_site.index_title = "Welcome to Nasscript Admin Panel"
+
+
+admin_site.register(User)
+admin_site.register(Category)
+admin_site.register(Dish)
+admin_site.register(Order)
+admin_site.register(OrderItem)
+admin_site.register(Bill)
+admin_site.register(Notification)
+admin_site.register(Floor)
+admin_site.register(Table)
+admin_site.register(Coupon)
 
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
@@ -34,9 +56,9 @@ class MenuAdmin(admin.ModelAdmin):
     )
     search_fields = ("name", "day_of_week", "mess_type__name", "created_by")
 
-admin.site.register(MenuItem)
-admin.site.register(Mess)
-admin.site.register(MessType)
+admin_site.register(MenuItem)
+admin_site.register(Mess)
+admin_site.register(MessType)
 
-admin.site.register(CreditUser)
-admin.site.register(CreditOrder)
+admin_site.register(CreditUser)
+admin_site.register(CreditOrder)
